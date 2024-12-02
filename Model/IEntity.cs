c441ff5511c640cb2beab7e1b5f28d321a9b04b2
vlcp197 <vlcp197@gmail.com>
2024-12-01 22:17:49 -0300

    Build the interface from where all entities in the game will inherit

---
+++ Model/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Asteroids.Model
+{
+    // This is the skeleton for all entities in the game: player, rocks, projectiles etc...
+    public interface IEntity
+    {
+        EntityType Type { get; }
+        Vector2 Position { get; }
+        bool isDestroyed { get; }
+
+        void Update(GameTime gameTime);
+    }
+}
