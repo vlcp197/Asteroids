941527b12598d6ae4effa655934e5a17453b144b
vlcp197 <vlcp197@gmail.com>
2024-12-01 22:20:11 -0300

    All entities will have an counterpart that draws them in the screen. This is the interface for those counterpart

---
+++ View/IEntityDrawer.cs
@@ -0,0 +1,18 @@
+﻿using Asteroids.Model;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Asteroids.View
+{
+    // This is the skeleton for all classes that will draw something on the screen. May it be the player, the asteroids, the laser beam, etc.
+    public interface IEntityDrawer<in T> where T: IEntity
+    {
+        EntityType Type { get; }
+
+        void LoadContent(ContentManager contentManager);
+        void Draw(SpriteBatch spriteBatch, IEntity entity);
+        void Draw(SpriteBatch spriteBatch, T entity);
+
+
+    }
+}
