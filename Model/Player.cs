a612680394f8b89ca13298500c8f27090c5232d4
vlcp197 <vlcp197@gmail.com>
2024-12-01 22:19:05 -0300

    The player class that inherits from IEntity

---
+++ Model/Player.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+
+namespace Asteroids.Model
+{
+    public class Player : IEntity
+    {
+        public EntityType Type => EntityType.Player;
+
+        public Vector2 Position { get; }
+        public bool isDestroyed { get;}
+
+        public Player(){}
+
+        
+        public void Update(GameTime gameTime) { }
+
+
+    }
+}
