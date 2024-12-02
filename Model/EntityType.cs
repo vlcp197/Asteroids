c9ef7972a1163361d2d148bb95909f37a8b08726
vlcp197 <vlcp197@gmail.com>
2024-12-01 22:18:10 -0300

    This is the list of the entities in the game

---
+++ Model/EntityType.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Asteroids.Model
+{
+    // I don't know why this is implemented, but as soon as i discover i will change that comment.
+    public enum EntityType
+    {
+        None = 0,
+        Player = 1,
+        Asteroid = 2,
+        LaserProjectile = 3
+    }
+}
