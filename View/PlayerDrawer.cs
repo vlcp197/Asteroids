1d27effbe4ed6fbf6147e2fd25baceda393ece9c
vlcp197 <vlcp197@gmail.com>
2024-12-01 22:20:38 -0300

    This is the class that draws the spaceship on the screen

---
+++ View/PlayerDrawer.cs
@@ -0,0 +1,18 @@
+﻿using Asteroids.Model;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Asteroids.View
+{
+    public class PlayerDrawer : IEntityDrawer<Player>
+    {
+        // Still don't know why doing this ↓↓↓
+        public EntityType Type => EntityType.Player;
+
+        public void LoadContent(ContentManager contentManager){}
+        public void Draw(SpriteBatch spriteBatch, IEntity entity){}
+        public void Draw(SpriteBatch spriteBatch, Player player) {}
+
+    }
+}
